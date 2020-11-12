42da77caf838f745bf87e70ff83295c29d75d2b0
naomichi <naomichi.programming@gmail.com>
2020-11-12 16:17:59 +0900

    複数フィルター

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 //Vue.filterは、new Vueを実行するよりも前に定義する
-Vue.filter("number_format", function (val) {
-  return val.toLocaleString();
-});
+// Vue.filter("number_format", function (val) {
+//   return val.toLocaleString();
+// });
 
 var app = new Vue({
   el: "#app",
   data: {
     price: 1000,
+  },
+  filters: {
+    number_format: function (val) {
+      return val.toLocaleString();
+    },
+    unit: function (val) {
+      return val + "円";
+    },
   },
 });
