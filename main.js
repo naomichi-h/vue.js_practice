700b6e065ba3277184cf4dc3a132a2ff6b1acbed
naomichi <naomichi.programming@gmail.com>
2020-11-12 15:22:51 +0900

    テンプレートを初期化しました

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
 var app = new Vue({
   el: "#app",
-  data: {
-    products: [
-      { code: "A01", name: "プロダクトA" },
-      { code: "B01", name: "プロダクトB" },
-      { code: "C01", name: "プロダクトC" },
-    ],
-  },
+  data: {},
 });
