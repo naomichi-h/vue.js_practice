3b7575dcabca5e9a9f5ad1f19080948907249d3f
naomichi <naomichi.programming@gmail.com>
2020-11-13 17:57:52 +0900

    addEventListener使用

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 var app = new Vue({
   el: "#app",
   data: {
-    stock: 10,
+    //ウインドウのサイズ
+    width: window.innerWidth,
+    height: window.innerHeight,
   },
+  created: function () {
+    //イベントハンドラを登録
+    addEventListener("resize", this.resizeHandler);
+  },
+  //   beforeDestroy: function () {
+  //     removeEventListener("resize", this.resizeHandler);
+  //   },
   methods: {
-    onDeleteItem: function () {
-      this.stock--;
+    //イベントハンドラ
+    resizeHandler: function ($event) {
+      //現在のウインドウサイズでプロパティを更新
+      this.width = $event.target.innerWidth;
+      this.height = $event.target.innerHeight;
     },
   },
 });
